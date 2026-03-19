ae794b0d158045ca9e9237f278b84047645fa291
Jamie Bläsi <jamie.blaesi@gmail.com>
2026-03-19 11:31:37 +0100

    feat: add device controller

--- src/main/java/ch/bumfuzzle/controller/DeviceController.java
+++ src/main/java/ch/bumfuzzle/controller/DeviceController.java
@@ -1,9 +1,33 @@
 package ch.bumfuzzle.controller;
 
+import ch.bumfuzzle.entity.Device;
+import ch.bumfuzzle.entity.User;
+import ch.bumfuzzle.repository.DeviceRepository;
+import ch.bumfuzzle.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/device")
 public class DeviceController {
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/all")
+    public List<Device> findAll(@AuthenticationPrincipal Jwt jwt) {
+        User user = userRepository.findByKeycloakId(jwt.getSubject()).orElse(null);
+        if (user == null) {return null;}
+        return deviceRepository.findAllByUser(user).orElse(List.of());
+    }
 }
--- src/main/java/ch/bumfuzzle/repository/DeviceRepository.java
+++ src/main/java/ch/bumfuzzle/repository/DeviceRepository.java
@@ -1,7 +1,12 @@
 package ch.bumfuzzle.repository;
 
 import ch.bumfuzzle.entity.Device;
+import ch.bumfuzzle.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DeviceRepository extends JpaRepository<Device, Long> {
+    Optional<List<Device>> findAllByUser(User user);
 }
